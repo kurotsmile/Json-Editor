aaefe416179cf11196a637fae7e0905f8f7f9de8
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-03-20 02:20:30 +0700

    Add code server
    Add code server

--- script/Manager_Project.cs
+++ script/Manager_Project.cs
@@ -1,3 +1,4 @@
+using Carrot;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,9 @@ using UnityEngine.Networking;
 
 public class Manager_Project : MonoBehaviour
 {
+    [Header("Obj Main")]
+    public App app;
+
     private int length = 0;
     public Sprite sp_icon_open_project;
     public Sprite sp_icon_project_online;
@@ -18,24 +22,8 @@ public class Manager_Project : MonoBehaviour
 
     public void add_project(string s_name, string s_data)
     {
-        string id_user_login = this.GetComponent<App>().carrot.user.get_id_user_login();
-        if (id_user_login!= "")
-        {
-            WWWForm frm_add_project = this.GetComponent<App>().carrot.frm_act("add_project");
-            frm_add_project.AddField("project_name", s_name);
-            frm_add_project.AddField("project_data", s_data);
-            frm_add_project.AddField("project_user", id_user_login);
-            frm_add_project.AddField("project_lang", this.GetComponent<App>().carrot.user.get_id_user_login());
-            //this.GetComponent<App>().carrot.send(frm_add_project,act_add_project);
-        }
-        else
-        {
-            PlayerPrefs.SetString("p_name_" + length, s_name);
-            PlayerPrefs.SetString("p_data_" + length, s_data);
-            PlayerPrefs.SetString("p_date_" + length, System.DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
-            this.length++;
-            PlayerPrefs.SetInt("p_length", this.length);
-        }
+        string id_user_login = this.app.carrot.user.get_id_user_login();
+
     }
 
     private void act_add_project(string s_data)
@@ -48,21 +36,21 @@ public class Manager_Project : MonoBehaviour
     public void show_list()
     {
         bool is_login_user = false;
-        if (this.GetComponent<App>().carrot.user.get_id_user_login() != "") is_login_user = true;
+        if (this.app.carrot.user.get_id_user_login() != "") is_login_user = true;
 
         if (this.length == 0 && is_login_user == false)
         {
-            this.GetComponent<App>().carrot.show_msg(PlayerPrefs.GetString("open","Open project"),PlayerPrefs.GetString("no_project","You don't have any archived projects yet"),Carrot.Msg_Icon.Error);
+            this.app.carrot.show_msg(PlayerPrefs.GetString("open","Open project"),PlayerPrefs.GetString("no_project","You don't have any archived projects yet"),Carrot.Msg_Icon.Error);
             return;
         }
 
-        this.GetComponent<App>().carrot.Create_Box(PlayerPrefs.GetString("open", "Open project"), this.sp_icon_open_project);
+        this.app.carrot.Create_Box(PlayerPrefs.GetString("open", "Open project"), this.sp_icon_open_project);
         for (int i = this.length-1; i >=0; i--)
         {
             if (PlayerPrefs.GetString("p_name_" + i, "") != "")
             {
                 GameObject obj_project = Instantiate(this.Project_Item_Prefab);
-                //obj_project.transform.SetParent(this.GetComponent<App>().carrot.area_body_box);
+                //obj_project.transform.SetParent(this.app.carrot.area_body_box);
                 obj_project.transform.localScale = new Vector3(1f, 1f, 1f);
                 obj_project.GetComponent<Project_Item>().txt_name.text = PlayerPrefs.GetString("p_name_" + i);
                 obj_project.GetComponent<Project_Item>().txt_date.text = PlayerPrefs.GetString("p_date_" + i);
@@ -80,11 +68,10 @@ public class Manager_Project : MonoBehaviour
 
     private void get_list_project_online()
     {
-        string id_user_login = this.GetComponent<App>().carrot.user.get_id_user_login();
+        string id_user_login = this.app.carrot.user.get_id_user_login();
         if (id_user_login != "") {
-            WWWForm frm_list_project = this.GetComponent<App>().carrot.frm_act("list_project");
-            frm_list_project.AddField("project_user", id_user_login);
-            //this.GetComponent<App>().carrot.send(frm_list_project,show_list_project_online);
+            StructuredQuery q = new(this.app.carrot.Carrotstore_AppId);
+            q.Add_where("user_id", Query_OP.EQUAL, id_user_login);
         }
     }
 
@@ -95,7 +82,7 @@ public class Manager_Project : MonoBehaviour
         {
             IDictionary data_project = (IDictionary)list_project_online[i];
             GameObject obj_project = Instantiate(this.Project_Item_Prefab);
-           // obj_project.transform.SetParent(this.GetComponent<App>().carrot.area_body_box);
+           // obj_project.transform.SetParent(this.app.carrot.area_body_box);
             obj_project.transform.localScale = new Vector3(1f, 1f, 1f);
             obj_project.GetComponent<Project_Item>().txt_name.text = data_project["name"].ToString();
             obj_project.GetComponent<Project_Item>().txt_date.text = data_project["date"].ToString();
@@ -114,35 +101,34 @@ public class Manager_Project : MonoBehaviour
         this.project_item_temp = p_item;
         string s_data = PlayerPrefs.GetString("p_data_" + p_item.index_project);
         string s_name = PlayerPrefs.GetString("p_name_" + p_item.index_project);
-        this.GetComponent<App>().sel_project_index = p_item.index_project;
+        this.app.sel_project_index = p_item.index_project;
         this.show_project(s_name, s_data);
     }
 
     public void show_project(string s_name,string s_data)
     {
-        this.GetComponent<App>().clear_list_item_editor();
-        this.GetComponent<App>().txt_save_status.text = s_name;
+        this.app.clear_list_item_editor();
+        this.app.txt_save_status.text = s_name;
         IDictionary<string, object> thanh = (IDictionary<string, object>)Carrot.Json.Deserialize(s_data);
-        this.paser_obj(thanh, this.GetComponent<App>().get_root());
-        this.GetComponent<App>().update_option_list();
-        if (this.GetComponent<App>().get_index_sel_mode() == 2)
-            this.GetComponent<App>().show_code_json(true);
+        this.paser_obj(thanh, this.app.get_root());
+        this.app.update_option_list();
+        if (this.app.get_index_sel_mode() == 2)
+            this.app.show_code_json(true);
         else
-            this.GetComponent<App>().show_code_json(false);
+            this.app.show_code_json(false);
 
-        this.GetComponent<App>().carrot.close();
-        this.GetComponent<App>().ScrollRect_all_item_editor.verticalNormalizedPosition = 1f;
-        this.GetComponent<App>().carrot.play_sound_click();
-        this.GetComponent<App>().update_option_list_obj();
+        this.app.carrot.close();
+        this.app.ScrollRect_all_item_editor.verticalNormalizedPosition = 1f;
+        this.app.carrot.play_sound_click();
+        this.app.update_option_list_obj();
     }
 
     public void show_project_online(Project_Item p_item)
     {
         this.project_item_temp = p_item;
-        this.GetComponent<App>().sel_project_index = p_item.index_project;
-        WWWForm frm_get_project = this.GetComponent<App>().carrot.frm_act("get_project");
-        frm_get_project.AddField("project_id",p_item.s_id_online);
-        //this.GetComponent<App>().carrot.send(frm_get_project, act_show_project_online);
+        this.app.sel_project_index = p_item.index_project;
+        StructuredQuery q = new(app.carrot.Carrotstore_AppId);
+        q.Add_where("project_id",Query_OP.EQUAL,p_item.s_id_online);
     }
 
     public void act_show_project_online(string s_data_online)
@@ -156,11 +142,11 @@ public class Manager_Project : MonoBehaviour
         if (thanh is  object)
         foreach (var obj_js in thanh)
         {
-            GameObject item_editor = Instantiate(this.GetComponent<App>().prefab_obj_js);
-            item_editor.transform.SetParent(this.GetComponent<App>().area_all_item_editor);
+            GameObject item_editor = Instantiate(this.app.prefab_obj_js);
+            item_editor.transform.SetParent(this.app.area_all_item_editor);
             item_editor.transform.localScale = new Vector3(1f, 1f, 1f);
             item_editor.GetComponent<js_object>().txt_name.text = obj_js.Key;
-            float x_space = this.GetComponent<App>().space_x_item * js_father.index;
+            float x_space = this.app.space_x_item * js_father.index;
             item_editor.GetComponent<js_object>().index = js_father.index + 1;
             item_editor.GetComponent<js_object>().index_list = js_father.index_list + 1;
             item_editor.GetComponent<js_object>().area_body.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, x_space, item_editor.GetComponent<js_object>().area_body.rect.height);
@@ -233,23 +219,23 @@ public class Manager_Project : MonoBehaviour
             item_editor.GetComponent<js_object>().check_child_expanded();
             item_editor.GetComponent<js_object>().load_obj_default();
             js_father.add_child(item_editor);
-            this.GetComponent<App>().add_obj_list_main(item_editor);
+            this.app.add_obj_list_main(item_editor);
         }
     }
 
     private void add_item_array(string s_val,int index_item, js_object js_father)
     {
-        GameObject item_editor = Instantiate(this.GetComponent<App>().prefab_obj_js);
-        item_editor.transform.SetParent(this.GetComponent<App>().area_all_item_editor);
+        GameObject item_editor = Instantiate(this.app.prefab_obj_js);
+        item_editor.transform.SetParent(this.app.area_all_item_editor);
         item_editor.transform.localScale = new Vector3(1f, 1f, 1f);
-        float x_space = this.GetComponent<App>().space_x_item * js_father.index;
+        float x_space = this.app.space_x_item * js_father.index;
         item_editor.GetComponent<js_object>().index = js_father.index + 1;
         item_editor.GetComponent<js_object>().area_body.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, x_space, item_editor.GetComponent<js_object>().area_body.rect.height);
         item_editor.GetComponent<js_object>().load_obj("array_item", s_val, index_item);
         item_editor.GetComponent<js_object>().check_child_expanded();
         item_editor.GetComponent<js_object>().load_obj_default();
         js_father.add_child(item_editor);
-        this.GetComponent<App>().add_obj_list_main(item_editor);
+        this.app.add_obj_list_main(item_editor);
     }
 
     public void delete_project(int index)
@@ -262,8 +248,8 @@ public class Manager_Project : MonoBehaviour
         {
             this.length = 0;
             PlayerPrefs.DeleteKey("p_length");
-            this.GetComponent<App>().carrot.close();
-            this.GetComponent<App>().set_save_status_default();
+            this.app.carrot.close();
+            this.app.set_save_status_default();
         }
         else
             this.show_list();
@@ -278,12 +264,10 @@ public class Manager_Project : MonoBehaviour
 
     public void delete_project_online(string s_id_project)
     {
-        WWWForm frm_delete_project = this.GetComponent<App>().carrot.frm_act("delete_project");
-        frm_delete_project.AddField("project_id", s_id_project);
-        //this.GetComponent<App>().carrot.send(frm_delete_project, this.act_delete_project);
+        app.carrot.server.Delete_Doc(this.app.carrot.Carrotstore_AppId, s_id_project, Act_delete_project);
     }
 
-    private void act_delete_project(string s_data)
+    private void Act_delete_project(string s_data)
     {
         this.show_list();
     }
@@ -292,41 +276,36 @@ public class Manager_Project : MonoBehaviour
     {
         if (this.project_item_temp.s_id_online != "")
         {
-            WWWForm frm_update_project = this.GetComponent<App>().carrot.frm_act("update_project_data");
-            frm_update_project.AddField("project_id", this.project_item_temp.s_id_online);
-            frm_update_project.AddField("project_data", s_data);
-            //this.GetComponent<App>().carrot.send(frm_update_project,this.act_update_project_online);
+            StructuredQuery q = new(app.carrot.Carrotstore_AppId);
+            q.Add_where("project_id", Query_OP.EQUAL, this.project_item_temp.s_id_online);
         }
         else
         {
             PlayerPrefs.SetString("p_data_" + index_project, s_data);
             PlayerPrefs.SetString("p_date_" + index_project, System.DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
-            this.GetComponent<App>().carrot.show_msg(PlayerPrefs.GetString("p_update_success","Project update successful!"));
+            this.app.carrot.show_msg(PlayerPrefs.GetString("p_update_success","Project update successful!"));
         }
     }
 
     private void act_update_project_online(string s_data)
     {
-        this.GetComponent<App>().carrot.show_msg(PlayerPrefs.GetString("p_update_success", "Project update successful!"));
+        this.app.carrot.show_msg(PlayerPrefs.GetString("p_update_success", "Project update successful!"));
     }
 
     public void update_project_name(int id_project,string s_new_name)
     {
         PlayerPrefs.SetString("p_name_" + id_project, s_new_name);
-        this.GetComponent<App>().carrot.show_msg(PlayerPrefs.GetString("p_name_update_success","Project name update successful!"));
+        this.app.carrot.show_msg(PlayerPrefs.GetString("p_name_update_success","Project name update successful!"));
     }
 
     public void update_project_name_online(string id_project, string s_new_name)
     {
-        WWWForm frm_update_project = this.GetComponent<App>().carrot.frm_act("update_project_name");
-        frm_update_project.AddField("project_id", id_project);
-        frm_update_project.AddField("new_name", s_new_name);
-        //this.GetComponent<App>().carrot.send(frm_update_project, this.act_update_project_name_online);
+
     }
 
     private void act_update_project_name_online(string s_data)
     {
-        this.GetComponent<App>().carrot.show_msg(PlayerPrefs.GetString("p_name_update_success", "Project name update successful!"));
+        this.app.carrot.show_msg(PlayerPrefs.GetString("p_name_update_success", "Project name update successful!"));
     }
 
     public void show_project_last()
@@ -339,13 +318,13 @@ public class Manager_Project : MonoBehaviour
     public void upload_project(Project_Item p_item)
     {
         this.project_item_temp = p_item;
-        WWWForm frm_upload_project = this.GetComponent<App>().carrot.frm_act("upload_project");
+        IDictionary data = (IDictionary) Json.Deserialize("{}");
         string s_data = PlayerPrefs.GetString("p_data_" + p_item.index_project);
         string s_name = PlayerPrefs.GetString("p_name_" + p_item.index_project);
-        frm_upload_project.AddField("project_name", s_name);
-        frm_upload_project.AddField("project_data", s_data);
-        frm_upload_project.AddField("project_user", this.GetComponent<App>().carrot.user.get_id_user_login());
-        //this.GetComponent<App>().carrot.send(frm_upload_project,act_upload_project);
+        data["project_name"] = s_name;
+        data["project_data"] = s_data;
+        data["user_id"] = app.carrot.user.get_id_user_login();
+        this.app.carrot.server.Add_Document_To_Collection(app.carrot.Carrotstore_AppId, data["id"].ToString(), Json.Serialize(data));
     }
 
     private void act_upload_project(string s_data)
@@ -357,7 +336,7 @@ public class Manager_Project : MonoBehaviour
         this.project_item_temp.obj_btn_export_web.SetActive(true);
         this.project_item_temp.obj_btn_share.SetActive(true);
         this.project_item_temp.obj_btn_export_file.SetActive(true);
-        this.GetComponent<App>().carrot.show_msg(PlayerPrefs.GetString("p_upload_success","Successful online project hosting!"));
+        this.app.carrot.show_msg(PlayerPrefs.GetString("p_upload_success","Successful online project hosting!"));
     }
 
     public void set_new_project()
@@ -378,21 +357,21 @@ public class Manager_Project : MonoBehaviour
             if (www.result == UnityWebRequest.Result.Success)
             {
                 Debug.Log(www.downloadHandler.text);
-                this.GetComponent<App>().clear_list_item_editor();
-                this.GetComponent<App>().txt_save_status.text =PlayerPrefs.GetString("import_file", "Import file");
+                this.app.clear_list_item_editor();
+                this.app.txt_save_status.text =PlayerPrefs.GetString("import_file", "Import file");
                 IDictionary<string, object> obj_js = (IDictionary<string, object>)Carrot.Json.Deserialize(www.downloadHandler.text);
-                this.paser_obj(obj_js, this.GetComponent<App>().get_root());
-                this.GetComponent<App>().update_option_list();
-                if (this.GetComponent<App>().get_index_sel_mode() == 2)
-                    this.GetComponent<App>().show_code_json(true);
+                this.paser_obj(obj_js, this.app.get_root());
+                this.app.update_option_list();
+                if (this.app.get_index_sel_mode() == 2)
+                    this.app.show_code_json(true);
                 else
-                    this.GetComponent<App>().show_code_json(false);
+                    this.app.show_code_json(false);
 
-                this.GetComponent<App>().carrot.close();
-                this.GetComponent<App>().ScrollRect_all_item_editor.verticalNormalizedPosition = 1f;
-                this.GetComponent<App>().carrot.play_sound_click();
-                this.GetComponent<App>().update_option_list_obj();
-                this.GetComponent<App>().panel_import.SetActive(false);
+                this.app.carrot.close();
+                this.app.ScrollRect_all_item_editor.verticalNormalizedPosition = 1f;
+                this.app.carrot.play_sound_click();
+                this.app.update_option_list_obj();
+                this.app.panel_import.SetActive(false);
             }
         }
     }
